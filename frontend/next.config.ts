327df03b8f3e694fb5f4439b7826a676622f2123
azka13labib-ops <azka13labib@gmail.com>
2026-08-19 16:57:38 +0700

    fix: remove HSTS header to prevent local ip testing issues

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,10 +5,7 @@ const securityHeaders = [
     key: 'X-DNS-Prefetch-Control',
     value: 'on',
   },
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=63072000; includeSubDomains; preload',
-  },
+
   {
     key: 'X-Frame-Options',
     value: 'SAMEORIGIN',
